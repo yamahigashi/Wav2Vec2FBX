a1f89958bc8fb5b97d725e757639db8c0211754e
yamahigashi <yamahigashi@gmail.com>
2022-02-23 14:12:45 +0900

    Updated build script to copy arbitrary packages.

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 import sys
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need fine tuning.
-# "packages": ["os"] is used as example only
-build_exe_options = {"packages": ["os"], "excludes": []}
+
+build_exe_options = {
+    "packages": [
+        "os",
+        "torch",
+        "_soundfile_data",
+        "pydub"
+    ],
+    "zip_include_packages": [
+    ],
+    "excludes": ["tkinter"],
+    "include_files": [
+        ("assets/config.toml", "config.toml"),
+        ("build/lib", "lib"),
+        ("lib", "lib"),
+    ]
+}
 
 base = None
 
 setup(
     name="Wav2Vec2FBX",
+    packages=["Wav2Vec2FBX"],
+    package_dir={"Wav2Vec2FBX": "src/Wav2Vec2FBX"},
     version="0.1",
     description="Recognize speech file and convert it into animation FBX ",
     options={"build_exe": build_exe_options},
-    executables=[Executable("src/wav2vec2fbx.py", base=base)]
+    executables=[Executable("src/Wav2Vec2FBX/wav2vec2fbx.py", base=base, target_name="Wav2Vec2FBX")],
+    entry_points={'console_scripts': ['Wav2Vec2FBX = src.__main__:main']}
 )
